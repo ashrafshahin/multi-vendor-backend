0e977c54aea7a43b3ef623c89558180a67ea9917
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-05-19 05:18:48 +0100

    Swagger UI and Swagger JS Doc related work part-1...

---
+++ src/config/swagger.js
@@ -0,0 +1,40 @@
+const { version } = require('mongoose')
+const swaggerJsdoc = require('swagger-jsdoc')
+
+const options = {
+    defination: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Multivendor eCommerce API',
+            version: '1.0.0',
+            description: ' API for large scale multivendor eCommerce (MERN Stack) ',
+            contact: {
+                name: 'Md Ashraf Shahin',
+                email: 'ashraf.shahin.dev@gmail.com',
+            }
+        },
+        servers: [
+            //{  url: `http://ashrafshahin.com`  } real domain use...
+            {
+                url: `http://localhost:${process.env.PORT || 5000}`,
+                description: 'Development server use only'
+            },
+
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                }
+            }
+        }
+    },
+    apis: ['./src/routes/*.js', './src/controllers/*.js']
+}
+
+const specification = swaggerJsdoc(options)
+
+module.exports = specification
+
