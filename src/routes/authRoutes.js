8060f0484401bb3240de0e279d1840ec43ee89f8
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-05-19 07:11:38 +0100

    Swagger Correction... Rejex is On, Name, Email, Password must be Server error if Frontend fails to write correct Match...

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,42 +7,37 @@ const { protect, restrictTo } = require('../middlewares/authMiddleware');
 
 /**
  * @swagger
- * /api/v1/auth/register
- *  post
- *      summary: Register a new user (Customer or Vendor)
- *      tags: [Auth]
- *      requestBody:
- *          required: true
- *          content:
- *              application/json:
- *                  schema:
- *                      type: Object
- *                      required:
- *                          name
- *                          email
- *                          password
- *                      properties:
- *                          name:
- *                              type: string
- *                          email:
- *                              type: string
- *                          password:
- *                              type: string
- *                          role:
- *                              type: string
- *                              enum: [customer,vendor]
- *       responses:
- *              201:
- *                  description: User registration successful
- *              400:
- *                  description: Bad request
- *              500:
- *                  description: Server Error
- * 
- * 
- *          
- *          
- *  
+ * /api/v1/auth/register:
+ *   post:
+ *     summary: Register a new user (Customer or Vendor)
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *                 enum: [customer, vendor]
+ *     responses:
+ *       201:
+ *         description: User registration successful
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Server Error
  */
 
 router.post('/register', register);
